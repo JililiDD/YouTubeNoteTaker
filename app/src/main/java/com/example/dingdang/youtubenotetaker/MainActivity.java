ba8d8674a811a932f3ce1f214ae61aff99c91b89
Jili Li <dindanmao@hotmail.com>
2018-02-22 10:10:20 -0400

    - Feb,22,2018 - Jili - Initialize buttons in the main activity and add button onclicklistener to the guest login button

--- app/src/main/java/com/example/dingdang/youtubenotetaker/MainActivity.java
+++ app/src/main/java/com/example/dingdang/youtubenotetaker/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.dingdang.youtubenotetaker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button guestLogin, login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        guestLogin = (Button) findViewById(R.id.btnGuestLogin);
+        login = (Button) findViewById(R.id.btnLogin);
+
+        guestLogin.setOnClickListener(new View.OnClickListener() { // Navigate to GuestActivity
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), GuestActivity.class));
+            }
+        });
     }
 }
