a6d6fbfc1e01e569b64bcbeae5f32dee28b3b083
yiwei <yw349762@dal.ca>
2018-03-09 09:55:05 -0400

    update 2 open a new notebook

--- app/src/main/java/com/example/dingdang/youtubenotetaker/AfterLoginActivity.java
+++ app/src/main/java/com/example/dingdang/youtubenotetaker/AfterLoginActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class AfterLoginActivity extends AppCompatActivity {
 
     Button btnOpenNotebook, btnMyNotebooks;
@@ -33,6 +36,9 @@ public class AfterLoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //TODO: Fetch users notebooks from Firebase and display them in UserNotebooksActivity
+
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference();
             }
         });
 
