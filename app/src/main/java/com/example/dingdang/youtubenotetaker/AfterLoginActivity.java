1e0540f2238e9549f28456acae09c5eefa0e55e2
Dinesh Shenoy <dinesh@dal.ca>
2018-03-18 23:58:06 -0300

    DS - Fourth Commit Added signout button by creating menu -> logout_menu.xml

--- app/src/main/java/com/example/dingdang/youtubenotetaker/AfterLoginActivity.java
+++ app/src/main/java/com/example/dingdang/youtubenotetaker/AfterLoginActivity.java
@@ -3,6 +3,8 @@ package com.example.dingdang.youtubenotetaker;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 
@@ -39,5 +41,12 @@ public class AfterLoginActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.logout_menu, menu);
+        return true;
     }
 }
