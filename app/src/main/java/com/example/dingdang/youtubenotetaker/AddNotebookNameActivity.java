1456acfc21f39cd14877e7db1b8f71a1c183fec7
Jili Li <jilili44@gmail.com>
2018-04-02 11:18:37 -0300

    - April,2,2018 - Finished AddNotebookNameActivity

--- app/src/main/java/com/example/dingdang/youtubenotetaker/AddNotebookNameActivity.java
+++ app/src/main/java/com/example/dingdang/youtubenotetaker/AddNotebookNameActivity.java
@@ -2,12 +2,36 @@ package com.example.dingdang.youtubenotetaker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddNotebookNameActivity extends AppCompatActivity {
+
+    private EditText etNotebookNameInput;
+    private Button btnOpen, btnCancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_notebook_name);
+
+        etNotebookNameInput = findViewById(R.id.etNotebookNameInput);
+        btnOpen = findViewById(R.id.btnOpen);
+        btnCancel = findViewById(R.id.btnCancel);
+
+        btnOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
