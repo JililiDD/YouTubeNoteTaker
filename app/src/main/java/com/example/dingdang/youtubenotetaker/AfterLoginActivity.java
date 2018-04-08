71aded53b08b47ae23dafe5fca8d3d3f367e40b0
Jili Li <dindanmao@hotmail.com>
2018-04-08 10:27:12 -0300

    - April,8,2018 - CLeaned AfterLoginActivity

--- app/src/main/java/com/example/dingdang/youtubenotetaker/AfterLoginActivity.java
+++ app/src/main/java/com/example/dingdang/youtubenotetaker/AfterLoginActivity.java
@@ -1,13 +1,9 @@
 package com.example.dingdang.youtubenotetaker;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
